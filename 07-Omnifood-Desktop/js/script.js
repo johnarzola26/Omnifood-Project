23286e704f9b10884eb0c92c1c6532d42304fbe7
Arzola <JohnCarlo.Arzola@tyson.com>
2022-03-22 21:59:41 -0500

    Added sticky navigation bar

--- 07-Omnifood-Desktop/js/script.js
+++ 07-Omnifood-Desktop/js/script.js
@@ -54,6 +54,17 @@ allLinks.forEach(function (link) {
   });
 });
 
+///////////////////////////////////////////////////////////Sticky Navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(function () {}, {
+  // in the viewport
+  root: null,
+  threshold: 0,
+});
+obs.observe();
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
