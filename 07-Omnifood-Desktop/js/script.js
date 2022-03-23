56c45598dd963db887c5d4e610cdc8466ec7badb
Arzola <JohnCarlo.Arzola@tyson.com>
2022-03-22 21:33:03 -0500

    Implemented smooth scrolling
    Added some smooth scrolling java script code so whenever a user clicks the links, it will take the user to where the link is linked to.

--- 07-Omnifood-Desktop/js/script.js
+++ 07-Omnifood-Desktop/js/script.js
@@ -17,15 +17,42 @@ const currentYear = new Date().getFullYear();
 console.log(currentYear);
 yearEl.textContent = currentYear;
 
-// ///////////////////////////////////////////////////////////
-// Make mobile navigation work
+///////////////////////////////////////////////////////////// Make mobile navigation work
 
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
-})("nav-open");
+});
+
+////////////////////////////////////////////////////////////// Smooth Scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to the top smoothly
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
