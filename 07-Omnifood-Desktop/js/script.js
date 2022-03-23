d5008f020c2c3799ab2c976356ea75fdc4091fd1
Arzola <JohnCarlo.Arzola@tyson.com>
2022-03-22 20:43:35 -0500

    Worked on the navigation button for mobile query
    Worked on the navigation button for the mobile query. When the user clicks the menu button, a menu will show up.

--- 07-Omnifood-Desktop/js/script.js
+++ 07-Omnifood-Desktop/js/script.js
@@ -3,6 +3,29 @@ console.log("Hello World!");
 const myName = "John Arzola";
 const h1 = document.querySelector(".heading-primary");
 console.log(myName);
+console.log(h1);
+
+// h1.addEventListener("click", function () {
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
+
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
+yearEl.textContent = currentYear;
+
+// ///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+})("nav-open");
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
